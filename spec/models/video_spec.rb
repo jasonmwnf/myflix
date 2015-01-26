3e0f66bea5a198ab36fdc9d0c3cb579be2eb45e4
Jason Williams <jasonmw_nf@hotmail.com>
2015-01-26 12:10:38 -0330

    updated video and category tests

--- spec/models/video_spec.rb
+++ spec/models/video_spec.rb
@@ -6,4 +6,10 @@ describe Video do
     video.save 
     expect(Video.first).to eq(video)
   end
+
+  it "belongs to category" do 
+    cat = Category.create(name: "Comedies")
+    video = Video.create(title: "Archer", description: "Funny.", category: cat)
+    expect(video.category).to eq(cat)
+  end
 end
