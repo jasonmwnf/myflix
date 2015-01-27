87bfa9294ff17c089d08de89843d747480c04179
Jason Williams <jasonmw_nf@hotmail.com>
2015-01-27 19:29:25 -0330

    refactored testing to use shoulda matchers

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -1,16 +1,5 @@
 require 'spec_helper'
 
 describe Category do 
-  it "saves itself" do
-    cat = Category.new(name: "Documentaries")
-    cat.save 
-    expect(Category.first).to eq(cat)
-  end
-
-  it "has many videos" do
-    cat = Category.create(name: "comedies")
-    archer = Video.create(title: "Archer", description: "Funniest show on tv.", category: cat)
-    simpsons = Video.create(title: "Simpsons", description: "Doh!", category: cat)
-    expect(cat.videos).to eq([archer, simpsons])
-  end
+  it { should have_many(:videos)}
 end
--- spec/models/video_spec.rb
+++ spec/models/video_spec.rb
@@ -1,21 +1,7 @@
 require 'spec_helper'
 
 describe Video do 
-  it "saves itself" do 
-    video = Video.new(title: "Archer", description: "Funniest show on tv.")
-    video.save 
-    expect(Video.first).to eq(video)
-  end
-
-  it "belongs to category" do 
-    cat = Category.create(name: "Comedies")
-    video = Video.create(title: "Archer", description: "Funny.", category: cat)
-    expect(video.category).to eq(cat)
-  end
-
-  it "does not save without a title or description" do
-    video1 = Video.create(description: "video description")
-    video2 = Video.create(title: "video title")
-    expect(Video.count).to eq(0)
-  end
+  it { should belong_to(:category)}
+  it { should validate_presence_of(:title)}
+  it { should validate_presence_of(:description)}
 end
