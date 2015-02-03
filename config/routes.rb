b44cc4ba28ea3aaa9140b5df5afc159085aa5bea
Jason Williams <jasonmw_nf@hotmail.com>
2015-02-03 12:35:04 -0330

    added user model and updated routes and homepage

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Myflix::Application.routes.draw do
+  root 'users#index'
+
   get 'ui(/:action)', controller: 'ui'
   get '/home', to: "videos#index"
+  get '/register', to: "users#new"
+  get '/sign_in', to: "sessions#new"
 
   resources :videos, only: [:show]
   resources :categories, only: [:show]
