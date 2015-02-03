2e2e2c41bc5183d31ff9642463cf2ae959bba219
Jason Williams <jasonmw_nf@hotmail.com>
2015-02-02 21:46:06 -0330

    updated search functionality

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,8 +5,30 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+comedies = Category.create(name: "Comedies")
+dramas = Category.create(name: "Dramas")
 
 Video.create(title: "Futurama", description: "Funny space show", 
-  small_cover_url: "/public/tmp/futurama.jpg", large_cover_url: "/public/tmp/futurama.jpg")
+  small_cover_url: "/tmp/futurama.jpg", large_cover_url: "/tmp/futurama.jpg", category: comedies)
 Video.create(title: "Monk", description: "Show starting wings dude.", 
-  small_cover_url: "/public/tmp/monk.jpg", large_cover_url: "/public/tmp/monk_large.jpg")
+  small_cover_url: "/tmp/monk.jpg", large_cover_url: "/tmp/monk_large.jpg", category: dramas)
+Video.create(title: "Archer", description: "Funnier cartoon.",
+  small_cover_url: "/tmp/monk.jpg", large_cover_url: "/tmp/monk_large.jpg", category: comedies)
+Video.create(title: "Futurama", description: "Funny space show", 
+  small_cover_url: "/tmp/futurama.jpg", large_cover_url: "/tmp/futurama.jpg", category: comedies)
+Video.create(title: "Monk", description: "Show starting wings dude.", 
+  small_cover_url: "/tmp/monk.jpg", large_cover_url: "/tmp/monk_large.jpg", category: dramas)
+Video.create(title: "Archer", description: "Funnier cartoon.",
+  small_cover_url: "/tmp/monk.jpg", large_cover_url: "/tmp/monk_large.jpg", category: comedies)
+Video.create(title: "Futurama", description: "Funny space show", 
+  small_cover_url: "/tmp/futurama.jpg", large_cover_url: "/tmp/futurama.jpg", category: comedies)
+Video.create(title: "Monk", description: "Show starting wings dude.", 
+  small_cover_url: "/tmp/monk.jpg", large_cover_url: "/tmp/monk_large.jpg", category: dramas)
+Video.create(title: "Archer", description: "Funnier cartoon.",
+  small_cover_url: "/tmp/monk.jpg", large_cover_url: "/tmp/monk_large.jpg", category: comedies)
+Video.create(title: "Futurama", description: "Funny space show", 
+  small_cover_url: "/tmp/futurama.jpg", large_cover_url: "/tmp/futurama.jpg", category: comedies)
+Video.create(title: "Monk", description: "Show starting wings dude.", 
+  small_cover_url: "/tmp/monk.jpg", large_cover_url: "/tmp/monk_large.jpg", category: dramas)
+Video.create(title: "Archer", description: "Funnier cartoon.",
+  small_cover_url: "/tmp/monk.jpg", large_cover_url: "/tmp/monk_large.jpg", category: comedies)
