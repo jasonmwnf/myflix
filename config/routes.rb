56d5abd72c8359633c459297c7208085b90a629e
Jason Williams <jasonmw_nf@hotmail.com>
2015-02-04 12:29:30 -0330

    added authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,15 @@
 Myflix::Application.routes.draw do
-  root 'users#index'
+  root 'pages#front'
 
   get 'ui(/:action)', controller: 'ui'
   get '/home', to: "videos#index"
   get '/register', to: "users#new"
   get '/sign_in', to: "sessions#new"
   post '/sign_in', to: "sessions#create"
+  get 'sign_out', to: "sessions#destroy"
 
-  resources :users, except: [:index, :new]
+  resources :users, only: [:create]
   resources :videos, only: [:show]
   resources :categories, only: [:show]
+
 end
