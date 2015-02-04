bac4806e8b07a00d4eabfff6bc6ee134df5ab893
Jason Williams <jasonmw_nf@hotmail.com>
2015-02-04 12:42:40 -0330

    updated flash styling

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,7 @@ class SessionsController < ApplicationController
     user = User.where(email: params[:email]).first #can use find_by in Rails 4
     if user && user.authenticate(params[:password])
       session[:user_id] = user.id
-      flash[:info] = "You are signed in, enjoy!"
+      flash[:warning] = "You are signed in, enjoy!"
       redirect_to home_path
     else
       flash[:danger] = "Invalid email or password."
@@ -17,7 +17,7 @@ class SessionsController < ApplicationController
 
   def destroy
     session[:user_id] = nil
-    flash[:info] = "You are signed out."
+    flash[:warning] = "You are signed out."
     redirect_to root_path
   end
 end
