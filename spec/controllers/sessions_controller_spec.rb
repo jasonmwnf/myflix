cdfdf6560c5dd6c7aba53aed2ae155c8e4e9b2a0
Jason Williams <jasonmw_nf@hotmail.com>
2015-02-06 12:01:55 -0330

    added test for video controller

---
+++ spec/controllers/sessions_controller_spec.rb
@@ -0,0 +1,26 @@
+require 'spec_helper'
+
+describe SessionsController do 
+  describe "GET new" do
+    it "renders the new template for unauthenticated users" do 
+      get :new
+      expect(response).to render_template :new
+    end
+    it "redirects to the home page for authenticated users" do 
+      session[:user_id] = Fabricate(:user).id 
+      get :new
+      expect(response).to redirect_to home_path
+    end
+  end
+
+  describe "POST create" do
+    it "sets user"
+    context "has valid authentication" do
+
+    end
+
+    context "has invalid authentication" do 
+
+    end
+  end
+end
