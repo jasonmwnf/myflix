a5c0619ee00f63fb735f4ce353b8e7b874ac818c
Jason Williams <jasonmw_nf@hotmail.com>
2015-02-07 12:56:01 -0330

    started review functionality

--- Gemfile
+++ Gemfile
@@ -23,7 +23,7 @@ end
 group :development, :test do
   gem 'pry'
   gem 'pry-nav'
-  gem 'rspec-rails', '2.99'
+  gem 'rspec-rails', '3.2'
 end
 
 group :test do
--- spec/controllers/videos_controller_spec.rb
+++ spec/controllers/videos_controller_spec.rb
@@ -19,6 +19,15 @@ describe VideosController do
       expect(assigns(:video)).to eq(video)
     end
 
+    it "sets @reviews for authenticated users" do 
+      session[:user_id] = Fabricate(:user).id
+      video = Fabricate(:video)
+      review1 = Fabricate(:review, video: video)
+      review2 = Fabricate(:review, video: video)
+      get :show, id: video.id
+      expect(assigns(:reviews)).to contain_exactly(review1, review2)
+    end
+
     it "redirects the user to the sign in page if unauthenticated" do
       video = Fabricate(:video)
       get :show, id: video.id
