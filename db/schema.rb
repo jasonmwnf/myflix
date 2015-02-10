66c1028e76259e332159930e43f3004ace036ba7
Jason Williams <jasonmw_nf@hotmail.com>
2015-02-10 10:09:29 -0330

    updated reviews controller and its test

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150207140049) do
+ActiveRecord::Schema.define(version: 20150210125759) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,10 +23,12 @@ ActiveRecord::Schema.define(version: 20150207140049) do
   end
 
   create_table "reviews", force: true do |t|
-    t.integer "user_id"
-    t.integer "video_id"
-    t.text    "content"
-    t.integer "rating"
+    t.integer  "user_id"
+    t.integer  "video_id"
+    t.text     "content"
+    t.integer  "rating"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "users", force: true do |t|
