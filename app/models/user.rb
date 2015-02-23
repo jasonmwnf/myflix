14815a1f11ad6fb0f30fa146303eddf46f564bad
Jason Williams <jasonmw_nf@hotmail.com>
2015-02-23 11:49:58 -0330

    updated my queue functionality and testing

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ActiveRecord::Base
   has_secure_password validations: false
   has_many :reviews
-  has_many :queue_items
+  has_many :queue_items, -> { order "position" }
   
   validates :username, :email, :password, presence: true
   validates_uniqueness_of :email
