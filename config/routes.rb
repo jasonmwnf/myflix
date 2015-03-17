3e568dafb346322c619ac0677bb66a13743b9f13
Jason Williams <jasonmw_nf@hotmail.com>
2015-03-17 11:23:16 -0230

    Implemented the user profile page

--- config/routes.rb
+++ config/routes.rb
@@ -16,7 +16,7 @@ Myflix::Application.routes.draw do
     resources :reviews, only: [:create]
   end
 
-  resources :users, only: [:create]
+  resources :users, only: [:create, :show]
   resources :categories, only: [:show]
   resources :queue_items, only: [:create, :destroy]
   post 'update_queue', to: 'queue_items#update_queue'
