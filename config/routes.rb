68e12676eae39d1a99e1b8611d02f16d5e09c441
Jason Williams <jasonmw_nf@hotmail.com>
2015-03-27 14:50:53 -0230

    added token column to invitations table

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,11 @@ Myflix::Application.routes.draw do
   root 'pages#front'
 
   get 'ui(/:action)', controller: 'ui'
-  get '/home', to: "videos#index"
-  get '/register', to: "users#new"
-  get '/sign_in', to: "sessions#new"
-  post '/sign_in', to: "sessions#create"
+  get 'home', to: "videos#index"
+  get 'register', to: "users#new"
+  get 'register/:token', to: "users#new_with_invitation_token", as: 'register_with_token'
+  get 'sign_in', to: "sessions#new"
+  post 'sign_in', to: "sessions#create"
   get 'sign_out', to: "sessions#destroy"
   get 'my_queue', to: 'queue_items#index'
   get 'people', to: 'relationships#index'
