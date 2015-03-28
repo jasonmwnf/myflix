d9e22c4442080cf68fc9150b0c397d3aed3697e6
Jason Williams <jasonmw_nf@hotmail.com>
2015-03-28 13:33:31 -0230

    wrote feature test for user invitation and acceptance

--- spec/support/macros.rb
+++ spec/support/macros.rb
@@ -10,6 +10,10 @@ def sign_in(a_user=nil)
   click_button "Sign In"
 end
 
+def sign_out
+  visit sign_out_path
+end
+
 def click_on_video_on_homepage(video)
   find("a[href='/videos/#{video.id}']").click 
 end
