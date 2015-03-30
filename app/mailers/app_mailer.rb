32cc9a7425bc855ca622d34b38cdc6cb954be424
Jason Williams <jasonmw_nf@hotmail.com>
2015-03-30 16:51:13 -0230

    quick app_mailer fix

--- app/mailers/app_mailer.rb
+++ app/mailers/app_mailer.rb
@@ -1,17 +1,17 @@
 class AppMailer < ActionMailer::Base
   def send_welcome_email(user)
     @user = user
-    mail(to: admin_or_user(user.email), from: "info@myflix.com", subject: "Welcome to MyFlix!")
+    mail(to: admin_or_user(@user.email), from: "info@myflix.com", subject: "Welcome to MyFlix!")
   end
 
   def send_forgot_password(user)
     @user = user
-    mail(to: admin_or_user(user.email), from: "info@myflix.com", subject: "Please reset your password")
+    mail(to: admin_or_user(@user.email), from: "info@myflix.com", subject: "Please reset your password")
   end
 
   def send_invitation_email(invitation)
     @invitation = invitation
-    mail(to: admin_or_user(invitation.recipient_email), from: "info@myflix.com", subject: "Invitation to join MyFlix")
+    mail(to: admin_or_user(@invitation.recipient_email), from: "info@myflix.com", subject: "Invitation to join MyFlix")
   end
 
   private
