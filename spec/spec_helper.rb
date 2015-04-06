5de9e8e675e9ccb71b6d675215136d56a8f35d88
Jason Williams <jasonmw_nf@hotmail.com>
2015-04-06 17:01:07 -0230

    updated spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -71,6 +71,14 @@ RSpec.configure do |config|
   # https://relishapp.com/rspec/rspec-rails/v/3-0/docs
   config.infer_spec_type_from_file_location!
   config.before(:suite) do
+    DatabaseCleaner.clean_with(:truncation)
+  end
+
+  config.before(:each) do
+    DatabaseCleaner.strategy = :truncation
+  end
+
+  config.before(:each, :js => true) do
     DatabaseCleaner.strategy = :truncation
   end
 
